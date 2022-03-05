8c6adb5f6e0d1775be888d0d0f40e554dd69acd4
KieranHannagan <kieranhan1999@gmail.com>
2022-03-05 00:00:21 -0600

    bug fixed, new brew mysql and package.json

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'SET NULL'
+  onDelete: 'CASCADE'
 });
 
 // Categories have many Products
@@ -19,7 +19,7 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'tags',
+  // as: 'tags',
 
   foreignKey: 'product_id',
   onDelete: 'SET NULL'
